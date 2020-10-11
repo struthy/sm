4de4a1221778bb9d97398bb205f92273ffd634d8
Adam Struth <adamstruth@Adams-iMac.local>
2020-10-11 23:52:44 +0100

    grouping data by days - rob

--- src/js/apps/weather.vue
+++ src/js/apps/weather.vue
@@ -1,100 +1,79 @@
 <template>
   <div class="container">
     <h1>Get the Weather boy</h1>
-    <h2>{{ city }}</h2>
+    <h2>{{ cityName }}</h2>
 
-    <div class="weather__days">
-      <label v-for="(day, i) in distinctDayDates" :key="i">
-        <input type="radio" v-model="selectedType" :value="day" />
-        {{ day }}
-      </label>
+    <div v-for="day in daysOfForecasts">
+      <h5>{{ day.dateString }}</h5>
+      <ul>
+        <li v-for="forecast in day.forecasts" class="weather__details">
+          {{ forecast.dt | moment("HH:mm") }}
+          {{ forecast.weather[0].main }}
+          <img v-bind:src="forecast.weather[0].icon | iconUrl" />
+        </li>
+      </ul>
     </div>
-
-    <ul>
-      <li class="weather__details">
-        <!-- <ul>
-          <li v-for="(day, i) in days" :key="'B' + i">
-            {{ day | moment(" h:mm:ss a") }}
-          </li>
-        </ul> -->
-        <ul>
-          <li v-for="(description, i) in filteredDescriptions" :key="'A' + i">
-            {{ description }}
-          </li>
-        </ul>
-      </li>
-    </ul>
-
-    <!-- <p v-for="(temp, i) in temps" :key="'B' + i">
-      {{ temp }}
-    </p> -->
   </div>
 </template>
-
+ 
 <script>
 import axios from "axios";
 export default {
   data() {
     return {
-      allItems: [],
-      days: [],
-      times: [],
-      city: [],
-      weather: {},
-      temps: [],
-      descriptions: [],
+      cityName: "Glasgow",
+      appId: "63739a03352f0a8eb85911fac41e3c50",
+      cityData: {},
       selectedType: [],
+      descriptions: [],
+      daysOfForecasts: [],
     };
   },
-
-  // https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=63739a03352f0a8eb85911fac41e3c50
-
-  created() {
-    axios
-      .get(
-        `http://api.openweathermap.org/data/2.5/forecast?q=GLasgow&appid=63739a03352f0a8eb85911fac41e3c50`
-      )
-      .then((response) => {
-        this.allItems = response.data;
-        this.city = response.data.city.name;
-        this.days = response.data.list.map(function(x) {
-          return x.dt;
-        });
-        this.temps = response.data.list.map(function(x) {
-          return Math.round(x.main.temp);
-        });
-        this.descriptions = response.data.list.map(function(x) {
-          return x.weather[0].description;
-        });
-      });
-  },
-
   computed: {
-    distinctDayDates: function() {
-      return this.days
-        .map((x) => new Date(x * 1000).toDateString())
-        .filter((x, i, arr) => arr.indexOf(x) == i);
+    APIURL: function () {
+      return `http://api.openweathermap.org/data/2.5/forecast?q=${this.cityName}&appid=${this.appId}`;
     },
-
     isAllActive() {
       return this.selectedType === "All";
     },
-    filteredDescriptions: function() {
-      var _this = this;
-      return _this.descriptions.filter(function(x) {
+    filteredDescriptions: function () {
+      return this.descriptions.filter((x) => {
         return x;
       });
     },
   },
-
-  methods: {
-    // activeItem: function(category) {
-    //   if (this.selectedType === category) {
-    //     return "isActive";
-    //   } else {
-    //     return "";
-    //   }
-    // },
+  filters: {
+    iconUrl: function (value) {
+      return `http://openweathermap.org/img/w/` + value + ".png";
+    },
+  },
+  created() {
+    var _vm = this;
+    axios.get(this.APIURL).then((response) => {
+      _vm.cityData = response.data.city;
+      _vm.daysOfForecasts = response.data.list
+        // map all the list items to a new object with a datestring property to filter/groupby
+        // and a forecasts property with all the forecast information for that day
+        .map((x, i, arr) => ({
+          dateString: new Date(x.dt * 1000).toDateString(),
+          forecasts: arr.filter((y) => {
+            var d1 = new Date(x.dt * 1000).toDateString();
+            var d2 = new Date(y.dt * 1000).toDateString();
+            return d1 === d2;
+          }),
+        }))
+        // filter out the duplicate days
+        .filter(function (x, i, arr) {
+          var firstOfTheDayIndex = arr
+            .map((e) => e.dateString)
+            .indexOf(x.dateString);
+          return i === firstOfTheDayIndex;
+        });
+    });
   },
 };
 </script>
+
+
+
+
